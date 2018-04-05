b22380fd7a94dc6ecb7bb7bee56218e209349ee0
YSJ <shaojieyew@gmail.com>
2018-04-06 00:47:52 +0800

    Added TF_IDF query but commented out as default is more efficient

--- myapp/indexing_controller.py
+++ myapp/indexing_controller.py
@@ -2,16 +2,31 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from myapp.solr_manager import index_manager
 from django.http import JsonResponse
+from urllib.request import urlopen
+import simplejson
 # Create your views here.
 class IndexingView(TemplateView):
     
+    def getIndexCount():
+        url='http://localhost:8983/solr/irproject/select?facet.field=doctype&facet.query=doctype&facet=on&fl=id&indent=on&q=*:*&wt=json'
+        connection1 = urlopen(url)
+        resp = simplejson.load(connection1)
+        types={"Total Indexed Document":resp["response"]["numFound"]}
+        facet_type = resp["facet_counts"]["facet_fields"]["doctype"]
+        index=len(facet_type)-1
+        while(index>=0):
+            if not(str(facet_type[index]).isdigit()):
+                types[str(facet_type[index])] = facet_type[index+1]
+            index=index-1
+        return types
+        
     def index(request, **kwargs):
         indexManager = index_manager.IndexManager()
         if(indexManager.is_indexing()==1):
             status = "indexing" 
         else:
             status = "" 
-        return render(request, 'indexing.html', context={'status': status})
+        return render(request, 'indexing.html', context={'status': status, 'stats': IndexingView.getIndexCount()})
     def start_indexing(request, **kwargs):
         type = request.GET.get("indexing_type")
         indexManager = index_manager.IndexManager()
@@ -19,14 +34,14 @@ class IndexingView(TemplateView):
             indexManager.start_indexing()
         indexManager.start_indexing(type)
         #print(files)
-        return render(request, 'indexing.html', context={'status': "done"})
+        return render(request, 'indexing.html', context={'status': "done", 'stats': IndexingView.getIndexCount()})
     def is_indexing(request, **kwargs):
         indexManager = index_manager.IndexManager()
         if(indexManager.is_indexing()==1):
             status = "indexing" 
         else:
             status = "" 
-        return render(request, 'indexing.html', context={'status': status})
+        return render(request, 'indexing.html', context={'status': status, 'stats': IndexingView.getIndexCount()})
     def stop_indexing(request, **kwargs):
         indexManager = index_manager.IndexManager()
         indexManager.stop_indexing()
@@ -34,7 +49,7 @@ class IndexingView(TemplateView):
             status = "indexing" 
         else:
             status = "" 
-        return render(request, 'indexing.html', context={'status': status})
+        return render(request, 'indexing.html', context={'status': status, 'stats': IndexingView.getIndexCount()})
     def get_files(request, **kwargs):
         indexManager = index_manager.IndexManager()
         files = indexManager.get_non_indexed_file()
@@ -44,7 +59,7 @@ class IndexingView(TemplateView):
             status = "indexing" 
         else:
             status = "" 
-        return render(request, 'indexing.html', context={'status': status,'files':files})
+        return render(request, 'indexing.html', context={'status': status,'files':files, 'stats': IndexingView.getIndexCount()})
     
     def delete(request, **kwargs):  
         indexManager = index_manager.IndexManager()
@@ -53,7 +68,7 @@ class IndexingView(TemplateView):
             status = "indexing" 
         else:
             status = "" 
-        return render(request, 'indexing.html', context={'status': status,'deleted': 1})
+        return render(request, 'indexing.html', context={'status': status,'deleted': 1, 'stats': IndexingView.getIndexCount()})
     def check_indexing(request, **kwargs):
         indexManager = index_manager.IndexManager()
         if(indexManager.is_indexing()==1):
--- myapp/search_controller.py
+++ myapp/search_controller.py
@@ -1,9 +1,13 @@
+import time
+import datetime
 from urllib.request import urlopen
+import requests
 from django.utils.http import urlquote
 import simplejson
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.conf import settings
+from collections import Counter
 from myapp.preprocess import preprocess
 import myapp
 import json
@@ -15,6 +19,8 @@ import pysolr
 class Search_View(TemplateView):
     dir_path =  os.path.dirname(myapp.__file__)+'\\..'
     def search(request, **kwargs):
+        
+        start_time = time.time()
         #get query and page parameters from request###################################################
         rows = request.GET.get("r")
         if(rows is None):
@@ -34,6 +40,37 @@ class Search_View(TemplateView):
         #searchDocument#########################################################################
         response=Search_View.searchDocument(query,filter_query,page,rows,filter_query_companyname);
         document_found_count = response['response']['numFound']
+
+        #spellcheck#########################################################################
+        corrected = original_query
+        if(document_found_count<200 and len(original_query)>0):
+            spellcheckreq = 'http://localhost:8983/solr/irproject/spell?spellcheck.q='+original_query.replace(' ','%20')+'&spellcheck=true&fq='+filter_query+'&spellcheck.extendedResults=true&spellcheck.collate=true&wt=json'
+            
+            try:
+                connection1 = urlopen(spellcheckreq)
+                spellcheckedresult = simplejson.load(connection1)
+                if(spellcheckedresult['spellcheck']['correctlySpelled'] == False):
+                    if(len(spellcheckedresult['spellcheck']['collations'])>1):
+                        corrected = spellcheckedresult['spellcheck']['collations'][1]['collationQuery']
+                        #preprocessor = preprocess.PreprocessPipeline()
+                        #processed_query1 = preprocessor.process(original_query)
+                        #processed_query2 = preprocessor.process(corrected)
+                        #if(processed_query1==processed_query2):
+                        #    corrected=''
+
+            except Exception as e: 
+                print(e)
+        
+        #get documents of spellchecked query##############################################
+        is_corrected_result = 0
+        if(document_found_count==0):
+            response=Search_View.searchDocument(corrected,filter_query,page,rows,filter_query_companyname);
+            document_found_count = response['response']['numFound']
+            if(document_found_count>0):
+                is_corrected_result = 1
+            
+        
+        ##Load oringal json files#######################################################
         facet_company = response['facet_counts']['facet_fields']["facet_text_en_nosplit"]
         index=len(facet_company)-1
         while(index>=0):
@@ -65,8 +102,8 @@ class Search_View(TemplateView):
             facet_type.append("("+str(doctype_count[1])+") Reviews")
         if(doctype_count[2]>0):
             facet_type.append("("+str(doctype_count[2])+") Interviews")
-        if(len(facet_type)<=1):
-            facet_type=[]
+        #if(len(facet_type)<=1):
+            #facet_type=[]
         
         facet_word = response['facet_counts']['facet_fields']["spellchecktext"]
         index=len(facet_word)-1
@@ -83,36 +120,7 @@ class Search_View(TemplateView):
         if(len(facet_word)>15):
             facet_word = facet_word[:15]
         
-        #spellcheck#########################################################################
-        corrected = original_query
-        if(document_found_count<200 and len(original_query)>0):
-            spellcheckreq = 'http://localhost:8983/solr/irproject/spell?spellcheck.q='+original_query.replace(' ','%20')+'&spellcheck=true&fq='+filter_query+'&spellcheck.extendedResults=true&spellcheck.collate=true&wt=json'
-            
-            try:
-                connection1 = urlopen(spellcheckreq)
-                spellcheckedresult = simplejson.load(connection1)
-                if(spellcheckedresult['spellcheck']['correctlySpelled'] == False):
-                    if(len(spellcheckedresult['spellcheck']['collations'])>1):
-                        corrected = spellcheckedresult['spellcheck']['collations'][1]['collationQuery']
-                        #preprocessor = preprocess.PreprocessPipeline()
-                        #processed_query1 = preprocessor.process(original_query)
-                        #processed_query2 = preprocessor.process(corrected)
-                        #if(processed_query1==processed_query2):
-                        #    corrected=''
-
-            except Exception as e: 
-                print(e)
         
-        #get documents of spellchecked query##############################################
-        is_corrected_result = 0
-        if(document_found_count==0):
-            response=Search_View.searchDocument(corrected,filter_query,page,rows,filter_query_companyname);
-            document_found_count = response['response']['numFound']
-            if(document_found_count>0):
-                is_corrected_result = 1
-            
-        
-        ##Load oringal json files#######################################################
         all_words='';
         r = []
         for document in response['response']['docs']:
@@ -121,6 +129,7 @@ class Search_View(TemplateView):
                 with open(url, encoding='utf-8') as fh:
                     data = json.load(fh)
                 data["id"]=document['id']
+                data["score"]=document['score']
                 data["doctype"]=document['doctype']
                 if(data["company_name"][(len(data["company_name"])-11):] ==' Interviews' ):
                     data["company_name"] = data["company_name"][:(len(data["company_name"])-11)]
@@ -141,6 +150,8 @@ class Search_View(TemplateView):
                             data["logo"] = company_data["logo"]
                         except:
                             i=1
+                            
+                            
                 if 'title' in data and not(data["title"] is None):
                     data["search_title"].append(data["title"]);
                 if 'company_name' in data and not(data["company_name"] is None):
@@ -189,9 +200,13 @@ class Search_View(TemplateView):
                     data["search_description"].append(data["opinion2"]);
                 if 'opinion3' in data and not(data["opinion3"] is None):
                     data["search_description"].append(data["opinion3"]);
+
                 data["search_description"] = ' '.join(data["search_description"])   
                 
-                
+                if "datetime_s" in document:
+                    data["datetime"]=document["datetime_s"]
+                    data["datetime"] = time.strptime(data["datetime"][:19], "%Y-%m-%dT%H:%M:%S")
+                    data["datetime"] = time.strftime("%m/%d/%Y", data["datetime"])
                 all_words = all_words+data["search_description"] + " "
                 all_words = all_words+data["search_title"] + " "
                 
@@ -214,7 +229,6 @@ class Search_View(TemplateView):
                 keys1 = keys1+t;
         keys2 = list(set(keys2))
         keys3 = []
-        print(keys1)    
         for key1 in keys1:
             length = int(len(key1)*(-0.25));
             if length==0:
@@ -228,15 +242,14 @@ class Search_View(TemplateView):
                             processed_query2=processed_query2[0]
                         else:    
                             processed_query2 = key2
-                        print(key1+"=="+processed_query2)
                         if(key1==processed_query2): 
                             keys3.append(key2)
         keys3 = sorted(keys3, key=len)[::-1]       
         for doc in r:  
             try:          
                 for key in keys3:    
-                    doc["search_description"]=doc["search_description"].replace(key,'<span class="highlight">'+key+'</span>')
-                    doc["search_title"]=doc["search_title"].replace(key,'<span class="title-highlight">'+key+'</span>')
+                    doc["search_description"]=doc["search_description"].replace(key,'<span class="DEWDSVWER">'+key+'</span>')
+                    doc["search_title"]=doc["search_title"].replace(key,'<span class="EDCWSASDFESSADWE">'+key+'</span>')
                
             except Exception as e: 
                 print(e)    
@@ -274,21 +287,23 @@ class Search_View(TemplateView):
             if(len(pagination['links'])==1):
                 pagination['links']=[]
         #######################################################
-        
+        query_time = (time.time() - start_time)
         return render(request, 'results.html',
         context={'query': original_query,'results': r,'spellcorrect': corrected,
         'document_found_count':document_found_count,'is_corrected_result':is_corrected_result,'filter_query_companyname':filter_query_companyname,
-        'filter_query':filter_query,'pagination':pagination,'facet_company':facet_company,'facet_type':facet_type,'facet_word':facet_word})
+        'filter_query':filter_query,'pagination':pagination,'facet_company':facet_company,'facet_type':facet_type,'facet_word':facet_word,'query_time':query_time})
     
     def getSyno(query):
-        a=['firm', 'corpor', 'enterpris', 'compani', 'ventur']
-        b=['competitor', 'rival', 'challeng', 'oppon', 'oppos', 'adversari', 'antagonist', 'combat', 'enemi', 'foe', 'competit', 'opposit']
-        c=['job', 'post', 'appoint', 'role', 'occup', 'employ', 'duti', 'placement']
-        d=['industri', 'busi', 'trade', 'field', 'line']
-        e=['pro', 'strength', 'benefit', 'good']
+        a=['firm', 'corpor', 'enterpris', 'compani']
+        b=['competitor', 'rival', 'enemi', 'foe']
+        c=['job', 'appoint', 'role', 'occup', 'employ', 'duti']
+        d=['industri',  'trade', 'field']
+        e=['pro', 'strength', 'benefit']
         f=['con', 'weak', 'neg', 'disadvantag']
-        g=['enjoy','pleas','pleasur','nice','agreeabl','satisfi','gratifi','best','good','fun','happi','great','awesom','amaz','incred','impress','wonder','spectacular','astonish','unbeliev','magnific','incred']
-        h=['sad','disagre', 'irksom', 'troublesom', 'annoi', 'irrit', 'vexati', 'displeas', 'uncomfort', 'distress', 'nasti', 'horribl', 'appal', 'terribl', 'aw', 'dread', 'hate', 'detest', 'miser', 'abomin', 'execr', 'odiou', 'invidi', 'objection', 'offens', 'obnoxi', 'repugn', 'repuls', 'repel', 'revolt', 'disgust', 'distast', 'nauseat', 'unsavour']
+        g=['enjoy','pleas','pleasur','nice','agreeabl','satisfi','gratifi','fun','happi','awesom','incred','impress','astonish','magnific']
+        h=['sad','disagre','irksom','troublesom','annoi','irrit','vexati','displeas','uncomfort','distress','appal','aw','dread','hate','detest','offens','obnoxi','repugn','repuls','revolt','disgust','distast']
+        i=['great', 'good']
+        j=['bad','nasti', 'horribl', 'terribl','worst','miser']
         if(query in a):
             return a;
         if(query in b):
@@ -305,13 +320,17 @@ class Search_View(TemplateView):
             return g;
         if(query in h):
             return h;
+        if(query in i):
+            return i;
+        if(query in j):
+            return j;
         return None;
     
     def searchDocument(query,filter_query,page,rows,filter_query_companyname):
         if(len(query)>0):
-            #preprocess query
             preprocessor = preprocess.PreprocessPipeline()
             processed_query = preprocessor.process(query)
+            tfidf_query = Search_View.tfidf_ltclnc_query_builder(processed_query)
             
             company_name_boost = 10
             competitors_boost = 0.3
@@ -333,6 +352,7 @@ class Search_View(TemplateView):
                 cons_boost=3
            
             
+            params = []
             fieldboost = []
             if(len(processed_query)>0):
                 #Boolean Or operator
@@ -349,8 +369,14 @@ class Search_View(TemplateView):
                 fieldboost.append('review_description_tag^1')
                 fieldboost.append('title_tag^'+str(title_boost))
                 fieldboost.append('competitors_tag^'+str(competitors_boost))
+                #params.append('q=+'+tfidf_query)
+                #params.append('defType=func')
+                params.append('q=+'+query)
+                params.append('defType=dismax')
             else:
                 query = query.replace(' ','%20')
+                params.append('q=+'+query)
+                params.append('defType=dismax')
             fieldboost.append('adviceMgmt^1')
             fieldboost.append('company_name^'+str(company_name_boost))
             fieldboost.append('cons^'+str(cons_boost))
@@ -366,28 +392,69 @@ class Search_View(TemplateView):
             fieldboost.append('competitors^'+str(competitors_boost))
             fieldboost = '+'.join(fieldboost)  
                 
-            params = []
-            params.append('rows='+str(rows))
-            params.append('defType=dismax')
-            params.append('wt=json')
             params.append('qf='+fieldboost)
-            params.append('start='+str((int(page)-1)*int(rows)))
-            params.append('fq='+filter_query)
-            if(len(filter_query_companyname)>0):
-                params.append('fq=facet_text_en_nosplit:"'+urlquote(filter_query_companyname)+'"')
-            params.append('q=+'+query)
             params.append('mm='+str(int((len(processed_query))*0.75)))
         else:
             params = []
-            params.append('rows='+str(rows))
-            params.append('wt=json')
-            params.append('start='+str((int(page)-1)*int(rows)))
-            params.append('fq='+filter_query)
-            if(len(filter_query_companyname)>0):
-                params.append('fq=facet_text_en_nosplit:"'+urlquote(filter_query_companyname)+'"')
             params.append('q=*:*')
+            
+        params.append('start='+str((int(page)-1)*int(rows)))
+        params.append('wt=json')
+        params.append('rows='+str(rows))   
+        params.append('fq='+filter_query)
+        if(len(filter_query_companyname)>0):
+            params.append('fq=facet_text_en_nosplit:"'+urlquote(filter_query_companyname)+'"')
+        params.append('facet.field=facet_text_en_nosplit')
+        params.append('facet.field=spellchecktext')
+        params.append('facet.field=doctype')
+        params.append('fl=score,*')
+        params.append('facet.query='+query)
+        params.append('facet=on')
+        params.append('bf=recip(ms(NOW,datetime_s),3.16e-11,1,1)^5.0')
+            
         url_params = '&'.join(params)    
-        connection = urlopen('http://localhost:8983/solr/irproject/select?'+url_params+'&facet.field=facet_text_en_nosplit&facet.field=spellchecktext&facet.field=doctype&facet.query='+query+'&facet=on')
-        response = simplejson.load(connection)
-        return response
+        url = "http://localhost:8983/solr/irproject/select"
+        headers = {"content-type" : "application/x-www-form-urlencoded" }
+        response = requests.post(url,  data=url_params,  headers=headers)
+        response_data = response.json()
+        return response_data
+    
+    def tfidf_ltclnc_query_builder(query):
+        field = "all_tag"
+        arrays = Counter(query)
+        lnc=[]
+        ltc=[]
+        string='sum('
+        for key in arrays.keys():
+            lnc.append(Search_View.get_term_lnc(field,key))
+            ltc.append(Search_View.get_term_ltc(field,key,arrays[key]))
+        index=0
+        for key in arrays.keys():
+            string = string+'mul(if(eq('+lnc[index]+',0),0,div('+lnc[index]+','
+            string=string+'sqrt(sum('
+            j=0
+            for key1 in arrays.keys():
+                string = string + 'mul('+lnc[j]+','+lnc[j]+'),'
+                j=j+1
+            string=string[:-1]
+            string=string+')))),if(eq('+ltc[index]+',0),0,div('+ltc[index]+','
+            string=string+'sqrt(sum('
+            j=0
+            for key1 in arrays.keys():
+                string = string + 'mul('+ltc[j]+','+ltc[j]+'),'
+                j=j+1
+            string=string[:-1]
+            string=string+')))))'
+            index=index+1
+            string=string+','
+        string=string[:-1]
+        string=string+')'
+        return string
+        
+    def get_term_lnc(field,term):
+        return "if(eq(termfreq("+field+","+term+"),0),0,sum(1,log(termfreq("+field+","+term+"))))"
+    def get_term_ltc(field,term,query_tf):
+        return "mul(sum(1,log("+str(query_tf)+")),log(if(eq(docfreq("+field+","+term+"),0),1,div(numdocs(),docfreq("+field+","+term+")))))"
+       
+       
         
